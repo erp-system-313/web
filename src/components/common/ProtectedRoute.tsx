0601ddb57299a6d9da4fd09dc9344f375ba0c1f8
mazenmizo27 <M.Ahmed2375@nu.edu.eg>
2026-04-16 02:07:51 +0200

    feat: Add ProtectedRoute component for route authentication

---
+++ src/components/common/ProtectedRoute.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Navigate } from 'react-router-dom';
+
+interface ProtectedRouteProps {
+  children: React.ReactNode;
+}
+
+export const ProtectedRoute: React.FC<ProtectedRouteProps> = ({ children }) => {
+  const isAuthenticated = true;
+  
+  if (!isAuthenticated) {
+    return <Navigate to="/login" replace />;
+  }
+
+  return <>{children}</>;
+};
+
+export default ProtectedRoute;
